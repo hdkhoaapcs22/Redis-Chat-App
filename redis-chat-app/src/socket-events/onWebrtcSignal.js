f8c6e5bcb4d867feea5e6ed58a0eeddc0d4e99ec
hdkhoaapcs22 <hdkhoa22@apcs.fit.edu.vn>
2025-06-27 20:27:54 +0700

    fix participants

--- redis-chat-app/src/socket-events/onWebrtcSignal.js
+++ redis-chat-app/src/socket-events/onWebrtcSignal.js
@@ -1,6 +1,14 @@
 import {io} from '../../server.js';
 
 const onWebrtcSignal = async (data) => {
+
+     const participants = data?.ongoingCall?.participants;
+
+    if (!participants) {
+        console.error("Missing participants in ongoingCall:", data);
+        return;
+    }
+    
     if(data.isCaller){
         if(data.ongoingCall.participants.receiver.socketId){
             io.to(data.ongoingCall.participants.receiver.socketId).emit('webrtcSignal', data)
